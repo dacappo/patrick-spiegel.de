9047757808ea3713afe6e95f1f9526049ce79684
Patrick Spiegel <patrickspiegel@web.de>
2016-02-19 15:33:29 +0100

    Reindent and comment index.html

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+(function(global) {
+  function resize() {
+    $('section .cut').each(function() {
+      if ($(this).hasClass('cut-top-right'))
+        $(this).css('border-right-width', $(this).parent().width()/2 + "px");
+      else if ($(this).hasClass('cut-top-left'))
+        $(this).css('border-left-width', $(this).parent().width()/2 + "px");
+    });
+  }
+
+  global.addEventListener("resize", resize);
+  resize();
+})(window);
