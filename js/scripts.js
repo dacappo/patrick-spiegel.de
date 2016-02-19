5adcce473d02debfbbd8f56190a9291275de23bc
Patrick Spiegel <patrickspiegel@web.de>
2016-02-19 17:03:42 +0100

    Uncomment unnecessary flexslider hanndler that causes error

--- js/scripts.js
+++ js/scripts.js
@@ -106,29 +106,7 @@ $(window).load(function() {
 		touch: false,
 		pauseOnHover: true,
 		start: function() {
-			$.waypoints('refresh');
-		}
-	});
-
-	$('#servicesSlider').flexslider({
-		animation: "slide",
-		directionNav: false,
-		controlNav: true,
-		touch: true,
-		pauseOnHover: true,
-		start: function() {
-			$.waypoints('refresh');
-		}
-	});
-
-	$('#teamSlider').flexslider({
-		animation: "slide",
-		directionNav: false,
-		controlNav: true,
-		touch: true,
-		pauseOnHover: true,
-		start: function() {
-			$.waypoints('refresh');
+			//$.waypoints('refresh');
 		}
 	});
 
